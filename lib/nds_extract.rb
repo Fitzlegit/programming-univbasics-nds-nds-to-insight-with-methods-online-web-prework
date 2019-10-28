21e3ad9a08c13178723a02c3afb0d49ca1c2bb51
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-28 18:44:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ require 'pry'
 def directors_totals(nds)
   result = {}
   director_index = 0
-  while director_index < nds.size do
+  while director_index < nds.length do
     
     director = nds[director_index][:name]
     
