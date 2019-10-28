cae2f26da7a9d10c581a6603cfd3e0963db866bb
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-28 18:45:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
     
     director = nds[director_index][:name]
     
-    result[director] = gross_for_director(director)
+    result[director] += gross_for_director(director)
     
     director_index += 1
   end
